0d22c8e012961e821ff53331c801ac8231ec8bde
Philipp <hansephilipp+git@googlemail.com>
2026-03-23 19:27:28 +0100

    Setup play view with canvas and ui

--- src/Views/game/play.php
+++ src/Views/game/play.php
@@ -3,43 +3,60 @@ use App\Core\Localization;
 use App\Core\Asset;
 ?>
 
-<h1><?= $game->getName() ?></h1>
+<!-- 🎮 THREE.js Canvas -->
+<canvas id="game_canvas"></canvas>
 
-<p>
-    <?= Localization::get('game.play.current_player') ?>:
-    <?= $game->getCurrentPlayer()->getUsername() ?>
-</p>
+<!-- 🧩 UI -->
+<div id="ui">
 
-<div id="game_wrapper">
-    <canvas id="game_canvas"></canvas>
+    <!-- HUD -->
+    <div id="hud">
+        <div>
+            🎮 <?= $game->getName() ?>
+        </div>
+        <div>
+            <?= Localization::get('game.play.current_player') ?>:
+            <?= $game->getCurrentPlayer()->getUsername() ?>
+        </div>
+    </div>
+
+    <!-- Controls -->
+    <div id="controls">
+        <button id="btn-roll">
+            🎲 <?= Localization::get('game.play.roll_dice') ?>
+        </button>
+
+        <button id="btn-end">
+            ➡️ <?= Localization::get('game.play.pass_turn') ?>
+        </button>
+
+        <button id="btn-menu">☰</button>
+    </div>
+
+    <!-- Menu -->
+    <div id="menu">
+        <h2><?= Localization::get('game.play.menu') ?></h2>
+
+        <button id="btn-resume">
+            <?= Localization::get('game.play.resume') ?>
+        </button>
+
+        <br><br>
+
+        <button id="btn-exit">
+            <?= Localization::get('game.play.exit') ?>
+        </button>
+    </div>
+
 </div>
 
+<!-- 🔧 Game Config -->
 <script>
     window.GAME_CONFIG = {
-        game_id: "<?= $game->getId() ?>", 
+        game_id: "<?= $game->getId() ?>",
         user_id: "<?= $user->getId() ?>"
     };
 </script>
 
-<!--<script type="module" src="<?= Asset::asset('/js/loader.js') ?>"></script>-->
+<!-- 🎮 Game entry point -->
 <script type="module" src="<?= Asset::asset('/js/game.js') ?>"></script>
-
-<style>
-    body {
-        margin: 0;
-        overflow: hidden;
-    }
-
-    #game_wrapper {
-        /*
-        width: 500px;
-        height: 500px;
-        */
-        margin: 0 auto;
-    }
-    #game_canvas {
-        display: block;
-        width: 100%;
-        height: 100%;
-    }
-</style>
