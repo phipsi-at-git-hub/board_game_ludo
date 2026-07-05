5107b275c0cff75e363621cb4f16a702cdf8a73b
Philipp <hansephilipp+git@googlemail.com>
2026-07-05 19:29:42 +0200

    Add asset folder path definitions

--- bootstrap/paths.php
+++ bootstrap/paths.php
@@ -3,10 +3,17 @@
 
 // Define applications paths
 defined('BASE_PATH') || define('BASE_PATH', dirname(__DIR__));
+defined('CONFIG_PATH') || define('CONFIG_PATH', BASE_PATH . '/config');
+
 defined('SRC_PATH') || define('SRC_PATH', BASE_PATH . '/src');
 defined('VIEWS_PATH') || define('VIEWS_PATH', SRC_PATH . '/Views');
+
+
 defined('PUBLIC_PATH') || define('PUBLIC_PATH', BASE_PATH . '/public'); 
-defined('CONFIG_PATH') || define('CONFIG_PATH', BASE_PATH . '/config');
+defined('CSS_PATH') || define('CSS_PATH', PUBLIC_PATH . '/css'); 
+defined('IMG_PATH') || define('IMG_PATH', PUBLIC_PATH . '/img'); 
+defined('JS_PATH') || define('JS_PATH', PUBLIC_PATH . '/js'); 
+
 defined('TRANSLATIONS_PATH') || define('TRANSLATIONS_PATH', BASE_PATH . '/translations');
 
 defined('API_PATH') || define('API_PATH', '/api/game/');
